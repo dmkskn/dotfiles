b122450b6c525a74948d86826fc0985b78b542d1
dmkskn <dmksknn@gmail.com>
2019-07-25 14:28:54 +0300

    Add asking for MOVIE_DATABASE

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 
 read -p "Enter path to Developer folder: " DEVELOPER
 read -p "Enter the TMDb API key (https://www.themoviedb.org): " TMDB_API_KEY
+read -p "Enter path to Movie Database: " MOVIE_DATABASE
 
 rm -f ~/.bash_local
 touch ~/.bash_local
 echo "DEVELOPER=$DEVELOPER" >> ~/.bash_local
 echo "TMDB_API_KEY=$TMDB_API_KEY" >> ~/.bash_local
+echo "MOVIE_DATABASE=$MOVIE_DATABASE" >> ~/.bash_local
 
 make --silent
