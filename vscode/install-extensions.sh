03040bca3f6e03c6f75e819095188bd418eba456
dmkskn <dmksknn@gmail.com>
2019-08-02 09:00:52 +0300

    Add gi extension to vscode

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -6,3 +6,7 @@ fi
 if ! /usr/local/bin/code --list-extensions | grep -i editorconfig.editorconfig &> /dev/null; then
     /usr/local/bin/code --uninstall-extension editorconfig.editorconfig &> /dev/null
 fi
+
+if ! /usr/local/bin/code --list-extensions | grep -i rubbersheep.gi &> /dev/null; then
+    /usr/local/bin/code --uninstall-extension rubbersheep.gi &> /dev/null
+fi
