0ea3790e35c5e0ef5188b68a8ad41c8189872069
dmkskn <dmksknn@gmail.com>
2019-08-02 09:06:24 +0300

    Remove Makefile

--- install.sh
+++ install.sh
@@ -11,9 +11,32 @@ echo "export TMDB_API_KEY=$TMDB_API_KEY" >> ~/.bash_local
 echo "export MOVIE_DATABASE=$MOVIE_DATABASE" >> ~/.bash_local
 
 read -p "Install packages (y/n): " INSTALL_PACKAGES
-read -p "Install Macos settings (y/n): " INSTALL_MACOS_SETTINGS
+read -p "Install macOS settings (y/n): " INSTALL_MACOS_SETTINGS
 
 [ "$INSTALL_PACKAGES" == "y" ] && bash packages.sh
 [ "$INSTALL_MACOS_SETTINGS" == "n" ] && bash macos.sh
 
-make --silent
+rm -f ~/.aliases
+ln -s "`pwd`/bash/aliases" ~/.aliases
+
+rm -f ~/.completions
+ln -s "`pwd`/bash/completions" ~/.completions
+
+rm -f ~/.bashrc
+ln -s "`pwd`/bash/bashrc" ~/.bashrc
+
+rm -f ~/.bash_profile
+ln -s "`pwd`/bash/bash_profile" ~/.bash_profile
+
+rm -f ~/.vimrc
+ln -s "`pwd`/vim/vimrc" ~/.vimrc
+
+rm -f ~/.gitconfig
+ln -s "`pwd`/git/gitconfig" ~/.gitconfig
+
+rm -f ~/.pythonrc.py
+ln -s "`pwd`/python/pythonrc.py" ~/.pythonrc.py
+
+bash `pwd`/vscode/install-extensions.sh
+rm -f ~/Library/Application\ Support/Code/User/settings.json
+ln -s `pwd`/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
