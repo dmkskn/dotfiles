42bc392451170c1ba2e65785664cd2e70dd6546a
dmkskn <dmksknn@gmail.com>
2019-10-10 13:54:32 +0300

    remove movie database

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
+#!/usr/bin/env bash
 
 read -p "Enter path to Developer folder: " DEVELOPER
 read -p "Enter the TMDb API key (https://www.themoviedb.org): " TMDB_API_KEY
-read -p "Enter path to Movie Database: " MOVIE_DATABASE
 
 rm -f ~/.bash_local
 touch ~/.bash_local
 
 echo "export DEVELOPER=$DEVELOPER" >> ~/.bash_local
 echo "export TMDB_API_KEY=$TMDB_API_KEY" >> ~/.bash_local
-echo "export MOVIE_DATABASE=$MOVIE_DATABASE" >> ~/.bash_local
 
 read -p "Install packages (y/n): " INSTALL_PACKAGES
 read -p "Install macOS settings (y/n): " INSTALL_MACOS_SETTINGS
