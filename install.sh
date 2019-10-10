e8e891bec7479718b1992af0aaaa3363db90a916
dmkskn <dmksknn@gmail.com>
2019-10-10 16:03:11 +0300

    clean up

--- install.sh
+++ install.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
-read -p "Enter path to Developer folder: " DEVELOPER
-read -p "Enter the TMDb API key (https://www.themoviedb.org): " TMDB_API_KEY
-
 rm -f ~/.bash_local
 [ -f ~/Documents/.bash_local ] && ln -s ~/Documents/.bash_local ~/.bash_local || touch ~/.bash_local
-
-echo "export DEVELOPER=$DEVELOPER" >> ~/.bash_local
-echo "export TMDB_API_KEY=$TMDB_API_KEY" >> ~/.bash_local
-
-read -p "Install packages (y/n): " INSTALL_PACKAGES
-read -p "Install macOS settings (y/n): " INSTALL_MACOS_SETTINGS
-
-[ "$INSTALL_PACKAGES" == "y" ] && bash "`pwd`/packages/packages.sh"
-[ "$INSTALL_MACOS_SETTINGS" == "n" ] && bash "`pwd`/macos/macos.sh"
 
 rm -f ~/.profile
 ln -s "`pwd`/sh/profile" ~/.profile
@@ -45,3 +33,9 @@ ln -s `pwd`/vscode/settings.json ~/Library/Application\ Support/Code/User/settin
 
 rm -f ~/Library/Application\ Support/Code/User/snippets/django-snippets.code-snippets
 ln -s `pwd`/vscode/snippets/django-snippets.code-snippets ~/Library/Application\ Support/Code/User/snippets/django-snippets.code-snippets
+
+read -p "Install packages (y/n): " INSTALL_PACKAGES
+read -p "Install macOS settings (y/n): " INSTALL_MACOS_SETTINGS
+
+[ "$INSTALL_PACKAGES" == "y" ] && bash "`pwd`/packages/packages.sh"
+[ "$INSTALL_MACOS_SETTINGS" == "n" ] && bash "`pwd`/macos/macos.sh"
