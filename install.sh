52af2eaa050435a020dd7ae5c936e8dabeb0cd0b
dmkskn <dmksknn@gmail.com>
2019-10-10 15:58:27 +0300

    Add looking for bash_local in ~/Documents first

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ read -p "Enter path to Developer folder: " DEVELOPER
 read -p "Enter the TMDb API key (https://www.themoviedb.org): " TMDB_API_KEY
 
 rm -f ~/.bash_local
-touch ~/.bash_local
+[ -f ~/Documents/.bash_local ] && ln -s ~/Documents/.bash_local ~/.bash_local || touch ~/.bash_local
 
 echo "export DEVELOPER=$DEVELOPER" >> ~/.bash_local
 echo "export TMDB_API_KEY=$TMDB_API_KEY" >> ~/.bash_local
