4e1547f7286220d1e865bf4d2122d691422d34f1
dmkskn <dmksknn@gmail.com>
2020-04-15 17:40:45 +0300

    Update VSCode extentions

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,12 +1,20 @@
 
 if ! /usr/local/bin/code --list-extensions | grep -i ms-python.python &> /dev/null; then
-    /usr/local/bin/code --uninstall-extension ms-python.python &> /dev/null
+    /usr/local/bin/code --install-extension ms-python.python &> /dev/null
 fi
 
 if ! /usr/local/bin/code --list-extensions | grep -i editorconfig.editorconfig &> /dev/null; then
-    /usr/local/bin/code --uninstall-extension editorconfig.editorconfig &> /dev/null
+    /usr/local/bin/code --install-extension editorconfig.editorconfig &> /dev/null
 fi
 
 if ! /usr/local/bin/code --list-extensions | grep -i rubbersheep.gi &> /dev/null; then
-    /usr/local/bin/code --uninstall-extension rubbersheep.gi &> /dev/null
+    /usr/local/bin/code --install-extension rubbersheep.gi &> /dev/null
+fi
+
+if ! /usr/local/bin/code --list-extensions | grep -i bmalehorn.vscode-fish &> /dev/null; then
+    /usr/local/bin/code --install-extension bmalehorn.vscode-fish &> /dev/null
+fi
+
+if ! /usr/local/bin/code --list-extensions | grep -i dnicolson.binary-plist &> /dev/null; then
+    /usr/local/bin/code --install-extension bmalehorn.vscode-fish &> /dev/null
 fi
