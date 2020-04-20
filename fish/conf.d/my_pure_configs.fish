d97cd11dfee3ab0672650105e5c11194e89236a6
dmkskn <dmksknn@gmail.com>
2020-04-20 11:42:39 +0300

    Add pure prompt (fish)

---
+++ fish/conf.d/my_pure_configs.fish
@@ -0,0 +1,7 @@
+# my pure prompt configuraton
+
+set --universal pure_symbol_prompt ">"
+set --universal pure_symbol_reverse_prompt "<"
+set --universal pure_symbol_git_unpulled_commits "[unpulled]"
+set --universal pure_symbol_git_unpushed_commits "[unpushed]"
+set --universal pure_symbol_git_dirty "[dirty]"
