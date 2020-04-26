e67b78b7760841881c9a80fc7341349645e43765
dmkskn <dmksknn@gmail.com>
2020-04-26 16:25:32 +0300

    Update extensions for VSCode

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,20 +1,6 @@
-
-if ! /usr/local/bin/code --list-extensions | grep -i ms-python.python &> /dev/null; then
-    /usr/local/bin/code --install-extension ms-python.python &> /dev/null
-fi
-
-if ! /usr/local/bin/code --list-extensions | grep -i editorconfig.editorconfig &> /dev/null; then
-    /usr/local/bin/code --install-extension editorconfig.editorconfig &> /dev/null
-fi
-
-if ! /usr/local/bin/code --list-extensions | grep -i rubbersheep.gi &> /dev/null; then
-    /usr/local/bin/code --install-extension rubbersheep.gi &> /dev/null
-fi
-
-if ! /usr/local/bin/code --list-extensions | grep -i bmalehorn.vscode-fish &> /dev/null; then
-    /usr/local/bin/code --install-extension bmalehorn.vscode-fish &> /dev/null
-fi
-
-if ! /usr/local/bin/code --list-extensions | grep -i dnicolson.binary-plist &> /dev/null; then
-    /usr/local/bin/code --install-extension bmalehorn.vscode-fish &> /dev/null
-fi
+for EXTENSION in $(cat extensions.txt)
+do
+    if ! /usr/local/bin/code --list-extensions | grep -i $EXTENSION &> /dev/null; then
+    /usr/local/bin/code --install-extension $EXTENSION &> /dev/null
+    fi
+done
