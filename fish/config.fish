98822984f55fc07b599caadf7f84b81c2911b36d
dmkskn <dmksknn@gmail.com>
2020-09-29 14:13:58 +0300

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -7,17 +7,24 @@ set -x LC_ALL en_US.UTF-8
 # Don't write .pyc files
 set -x PYTHONDONTWRITEBYTECODE 1
 
+set -x PYTHONSTARTUP $HOME/.pythonstartup.py
+
 # Set vim as the default editor
 set -gx EDITOR vim
 
-# Pipx virtual enviroment location
-set fish_user_paths /Users/home/.local/bin $fish_user_paths
+# Pyenv
+set fish_user_paths $HOME/.pyenv $fish_user_paths
+
+# Pipx
+set fish_user_paths $HOME/.local/bin $fish_user_paths
 
 # Poetry
-set fish_user_paths /Users/home/.poetry/bin $fish_user_paths
+set fish_user_paths ~/.poetry/bin $fish_user_paths
 
 # lsregister
 set fish_user_paths "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support" $fish_user_paths
 
 # Pipenv
 set -x PIPENV_VENV_IN_PROJECT "enabled"
+
+source .tmdb_api_key
---
+++ python/pythonstartup.py
@@ -0,0 +1,5 @@
+import itertools
+import os
+import pathlib
+import subprocess
+import sys
