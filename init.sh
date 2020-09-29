24a52dc12c5fa1e4e852afabf2cf07d81b2a4045
dmkskn <dmksknn@gmail.com>
2020-09-29 16:11:06 +0300

    updates

--- init.sh
+++ init.sh
@@ -8,11 +8,13 @@ if [ "$(uname)" = "Darwin" ]; then
     [ -n $(brew tap | grep "homebrew/cask") ] && brew tap homebrew/cask
     [ -z $(which fish) ] && brew install fish
     [ -z $(which emacs) ] && brew install emacs
+    [ -z $(which vim) ] && brew install vim
     [ -z $(brew list --cask | grep iterm2) ] && brew cask install iterm2
     [ -z $(brew list --cask | grep visual-studio-code) ] && brew cask install visual-studio-code
 else # Linux
     [ -z $(which fish) ] && sudo apt-get install fish
     [ -z $(which emacs) ] && sudo apt-get install emacs
+    [ -z $(which vim) ] && sudo apt-get install vim
 fi
 
 exit 0
