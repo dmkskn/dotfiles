4f7a1865de30f93169fb61e9572e1b087f14660b
dmkskn <dmksknn@gmail.com>
2020-10-07 19:02:14 +0300

    add des and dev functions

---
+++ fish/functions/des.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function des --wraps='cd ~/Desktop' --description 'alias des cd ~/Desktop'
+    cd ~/Desktop $argv
+end
---
+++ fish/functions/dev.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function dev --wraps='cd ~/Developer' --description 'alias dev cd ~/Developer'
+    cd ~/Developer $argv
+end
