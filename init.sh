ba5cd049600996b2762888a249f72bdf84ed5739
Dima Koskin <dmkskn@icloud.com>
2021-06-09 10:40:09 +0300

    Remove emacs

--- init.sh
+++ init.sh
@@ -7,13 +7,11 @@ if [ "$(uname)" = "Darwin" ]; then
     [ -z $(which brew) ] && curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | /bin/bash
     [ -n $(brew tap | grep "homebrew/cask") ] && brew tap homebrew/cask
     [ -z $(which fish) ] && brew install fish
-    [ -z $(which emacs) ] && brew install emacs
     [ -z $(which vim) ] && brew install vim
     [ -z $(brew list --cask | grep iterm2) ] && brew install --cask iterm2
     [ -z $(brew list --cask | grep visual-studio-code) ] && brew install --cask visual-studio-code
 else # Linux
     [ -z $(which fish) ] && sudo apt-get install fish
-    [ -z $(which emacs) ] && sudo apt-get install emacs
     [ -z $(which vim) ] && sudo apt-get install vim
 fi
 
