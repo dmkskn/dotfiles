b04b082581b26c4300d7fe5a5fdced25ff7f6512
Dima Koskin <dmkskn@icloud.com>
2021-06-09 10:53:52 +0300

    Install programming languages

--- init.sh
+++ init.sh
@@ -6,8 +6,14 @@ if [ "$(uname)" = "Darwin" ]; then
     [ -d $(xcode-select --print-path) ] || xcode-select --install
     [ -z $(which brew) ] && curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | /bin/bash
     [ -n $(brew tap | grep "homebrew/cask") ] && brew tap homebrew/cask
+    
     [ -z $(which fish) ] && brew install fish
     [ -z $(which vim) ] && brew install vim
+
+    [ -n $(brew list | grep python) ] && brew install python
+    [ -z $(brew list --cask | grep julia) ] brew install --cask julia
+    [ -n $(brew list | grep node) ] && brew install node
+
     [ -z $(brew list --cask | grep iterm2) ] && brew install --cask iterm2
     [ -z $(brew list --cask | grep visual-studio-code) ] && brew install --cask visual-studio-code
 else # Linux
